e4371be662295db752f351651f17ac39012db7f0
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2023-11-24 07:48:51 -0300

    Agora ta tudo pronto kk

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,36 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package principal;
 
 import java.sql.SQLException;
 import gui.Login;
 
-/**
- *
- * @author Manhã
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SQLException{   //Teste adicionando no banco
-        /*
-        Connection connection = new ConnectionAcademia().getConnection();
-        System.out.println("Conexão aberta!");
-        connection.close();*/
-        
-        /*Data dat1 = new Data(25, 07, 2004);
-        Aluno aluno1 = new Aluno("Arthur", "53101976804", dat1, 45.5, 1.75);
-        
-        AlunoDAO add = new AlunoDAO(); 
-        add.adicionaAluno(aluno1);*/
-        
+    public static void main(String[] args) throws SQLException{
+
         new Login();
-        
-        //new AlterarPeso("53101976804");
         
         
     }
